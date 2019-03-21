499e52b7aebe8c7e7221f31e685b0cd35359e8f6
Krishna Solanki <kasolanki06@gmail.com>
2019-03-20 22:58:23 -0400

    registration complete

---
+++ client/src/components/Header.vue
@@ -0,0 +1,16 @@
+<template>
+  <v-toolbar fixed class="teal" dark>
+      <v-toolbar-title class="mr-4">
+        FitnessTracker
+      </v-toolbar-title>
+  </v-toolbar>
+</template>
+
+<script>
+export default {
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ server/src/controllers/AuthenticationController.js
@@ -0,0 +1,15 @@
+const { User } = require('../models')
+
+module.exports = {
+  async register (req, res) {
+    try{
+      console.log('test')
+      const user = await User.create(req.body)
+      res.send(user.toJSON())
+    } catch (err) {
+      res.status(400).send({
+        error: 'This email account is already in use.'
+      })
+    }
+  }
+}
