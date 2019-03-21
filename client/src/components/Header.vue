141781ed825469099b036102e1c67cbcc902aa13
Krishna Solanki <kasolanki06@gmail.com>
2019-03-21 17:44:14 -0400

    login

--- client/src/components/Header.vue
+++ client/src/components/Header.vue
@@ -1,16 +1,56 @@
 <template>
   <v-toolbar fixed class="teal" dark>
       <v-toolbar-title class="mr-4">
-        FitnessTracker
+        <span
+          class="home"
+          @click="navigateTo({name: 'root'})">
+          FitnessTracker
+        </span>
       </v-toolbar-title>
+
+      <!--<v-toolbar-items>
+        <v-btn flat dark>
+          Browse
+        </v-btn>
+      </v-toolbar-items> -->
+
+      <v-spacer></v-spacer>
+
+      <v-toolbar-items>
+       <!-- <router-link to="register"> -->
+
+          <v-btn class="amber" flat dark
+            @click="navigateTo({name: 'login'})">
+            Log In
+          </v-btn>
+
+          <v-btn class="amber" flat dark
+            @click="navigateTo({name: 'register'})">
+            Sign Up
+          </v-btn>
+
+       <!-- </router-link> -->
+      </v-toolbar-items>
   </v-toolbar>
 </template>
 
 <script>
 export default {
+  methods: {
+    navigateTo (route) {
+      this.$router.push(route)
+    }
+  }
 }
 </script>
 
 <style scoped>
+.home{
+    cursor: pointer;
+}
+
+.home:hover{
+    color: brown;
+}
 
 </style>
