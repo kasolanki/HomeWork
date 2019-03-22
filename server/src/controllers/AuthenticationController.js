4d9f07d9b7766d81c3142dd19ea9cafc89303a05
Krishna Solanki <kasolanki06@gmail.com>
2019-03-22 02:51:04 -0400

    bcrypt password

--- server/src/controllers/AuthenticationController.js
+++ server/src/controllers/AuthenticationController.js
@@ -14,7 +14,11 @@ module.exports = {
     try{
       console.log('test')
       const user = await User.create(req.body)
-      res.send(user.toJSON())
+      const userJson = user.toJSON()
+      res.send({
+        user: userJson,
+        token: jwtSignUser(userJson)
+      })
     } catch (err) {
       res.status(400).send({
         error: 'This email account is already in use.'
@@ -37,7 +41,7 @@ module.exports = {
         })
       }
 
-      const isPasswordValid = password ===user.password
+      const isPasswordValid = await user.comparePassword(password)
       if(!isPasswordValid) {
         return res.status(403).send({
           error: 'The login information was incorrect.'
